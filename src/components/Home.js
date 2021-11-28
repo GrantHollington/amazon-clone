2f5e3f2cb102b2b48a53ddbeb48a4f898129360a
GrantHollington <grant.j.hollington@gmail.com>
2021-11-28 16:23:42 +1100

    create home component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Home.css';
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://m.media-amazon.com/images/I/61Q6BYGdNxL._SX3000_.jpg" alt="Cyber deals" />
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                 <div className="home__row">
+                    {/* Product */}
+                </div>
+            </div>
+        </div>
+    )
+}
