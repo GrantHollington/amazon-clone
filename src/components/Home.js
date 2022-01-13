07bd1301c336d2a190bdcb15e8584b66cf7c6478
GrantHollington <grant.j.hollington@gmail.com>
2022-01-13 17:56:42 +1100

    add price, rating, image and button to product page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css';
+import Product from './Product';
 
 export default function Home() {
     return (
@@ -8,7 +9,7 @@ export default function Home() {
                 <img className="home__image" src="https://m.media-amazon.com/images/I/61Q6BYGdNxL._SX3000_.jpg" alt="Cyber deals" />
 
                 <div className="home__row">
-                    {/* Product */}
+                    <Product />
                     {/* Product */}
                 </div>
 
---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Product.css'
+
+function Product() {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>The lean startup</p>
+                <p className="product_price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product_rating">
+                    <p>⭐️</p>
+                    <p>⭐️</p>
+                    <p>⭐️</p>
+                </div>
+            </div>
+            <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1394265182l/12969026.jpg" alt="" />
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
