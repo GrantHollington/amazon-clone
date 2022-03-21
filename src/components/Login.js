a0b0d225d3e4146bf2f6055110176a1d59c16f24
GrantHollington <grant.j.hollington@gmail.com>
2022-03-21 18:06:07 +1100

    add styling and useState functionality to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
 
 function Login() {
+    // useState for email and password. useState is like variables in react
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        // this prevents react from reloading page
+        e.preventDefault();
+
+        // do some fancy firebase login tuff here
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        // do some fancy firebase regster stuff here
+    }
+
   return (
       <div className='login'>
         <Link to='/'>
@@ -15,19 +32,20 @@ function Login() {
 
             <form action="">
                 <h5>E-mail</h5>
-                <input type="text" />
+                {/* value and onChange to attach user inputs to store in useState */}
+                <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
 
                 <h5>Password</h5>
-                <input type="password" />
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
 
-                <button className='login__signInButton'>Sign In</button>
+                <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
             </form>
 
             <p>
-                By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use and Sale.Please see our Privacy Notice, our Cookies Notice and our Interest Based Ads
+                By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use and Sale.Please see our Privacy Notice, our Cookies Notice and our Interest Based Ads Ntoice.
             </p>
 
-            <button className='login__registerButton'>Create your account</button>
+            <button onClick={register} className='login__registerButton'>Create your account</button>
         </div>
     </div>
 
